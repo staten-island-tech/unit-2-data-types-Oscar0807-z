f9c87bc2364cc26ecb044bcf700b1ef4a6f0a83b
Oscar0807-z <oscarz43@nycstudents.net>
2026-02-02 09:25:56 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+
+tip = input("How much did you tip")
+if int(tip) == 25:\
+    print ('great')
+elif int(tip) == 20:
+     print ('good')
+elif tip == 15:
+     print ('okay')
+elif tip == 10:
+     print ('bad')
